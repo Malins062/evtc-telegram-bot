d2cb2f8b9a081b0dc1a04ef049f2e85090e42b95
Alexei Malinovskiy <6328673@gmail.com>
2024-05-24 15:37:24 +0300

    feat: add commands

--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import asyncio
+import logging
 
 from aiogram import Dispatcher, Bot, types
+from aiogram.filters import CommandStart, Command
 
-BOT_TOKEN = '7005910963:AAGPlaw25ivbytySZWlHX7v9F7Hqn5QtXQo'
+import config
 
-bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
 
+@dp.message(CommandStart())
+async def handle_start(message: types.Message):
+    await message.answer(text=f'Привет, {message.from_user.full_name}!')
+
+
+@dp.message(Command('help'))
+async def handle_help(message: types.Message):
+    text = 'I\'m and echo bot.\nSend me any message!'
+    await message.answer(text=text)
+
+
+@dp.message()
+async def echo_message(message: types.Message):
+    # await bot.send_message(
+    #     chat_id=message.chat.id,
+    #     text='Start processing...',
+    # )
+    # await bot.send_message(
+    #     chat_id=message.chat.id,
+    #     text='Detected message...',
+    #     reply_to_message_id=message.message_id,
+    # )
+
+    await message.answer(
+        text='Wait a second...',
+    )
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Something new 🙂')
+
+
 async def main():
-    await dp.start_polling()
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=config.BOT_TOKEN)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
     asyncio.run(main())
