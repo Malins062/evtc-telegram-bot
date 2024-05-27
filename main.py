0227dc8cdc24a9485871a7924396434b2aadaedd
Malins062 <6328673@gmail.com>
2024-05-27 20:52:57 +0300

    refactor: optimization

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from aiogram.enums import ParseMode
 import config
 from routers import router as main_router
 
-dp = Dispatcher()
-
-dp.include_router(main_router)
-
 
 async def main():
+    dp = Dispatcher()
+    dp.include_router(main_router)
+
     logging.basicConfig(level=logging.INFO)
     bot = Bot(
         token=config.BOT_TOKEN,
         parse_mode=ParseMode.HTML,
     )
+
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
