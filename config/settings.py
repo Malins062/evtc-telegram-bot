3c88a6aabdcc87c600f173a226b99fa109aba327
Alexei Malinovskii 209 <6328673@gmail.com>
2024-06-18 14:51:56 +0300

    feat: added new conditions validate date

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,10 @@ class Settings(BaseSettings):
     port: int = os.getenv('PORT')
     use_tls: bool = os.getenv('USE_TLS')
 
+    # DateTime settings
     time_zone: str = 'Europe/Moscow'
+    datetime_format: str = '%d.%m.%Y %H:%M'
+    datetime_delta: int = 8
 
     prefix: str = '!/\\'
 
--- utils/common.py
+++ utils/common.py
@@ -4,8 +4,6 @@ from pytz import timezone
 from config.settings import settings
 
 
-# def get_now(utc=settings.time_utc) -> str:
 def get_now() -> str:
     now = dt.datetime.now(timezone(settings.time_zone))
-    # now += dt.timedelta(hours=utc)
-    return now.strftime('%d.%m.%Y %H:%M')
+    return now.strftime(settings.datetime_format)
