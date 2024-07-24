017dadbf1c6749abb99249453b2c5a98d9579f29
63286 <6328673@gmail.com>
2024-07-24 16:30:23 +0300

    refactor: added redis models

--- evtc_bot/db/redis/engine.py
+++ evtc_bot/db/redis/engine.py
@@ -2,5 +2,9 @@ from aiogram.fsm.storage.redis import RedisStorage
 
 from evtc_bot.config.settings import settings
 
+
+CHECKED_USERS = "checked_users"
+
+
 redis_storage = RedisStorage.from_url(settings.db.redis_url)
 # storage = RedisStorage.from_url(f"redis://{settings.redis_user}:{settings.redis_pswd}@{settings.redis_host}:22/0")
---
+++ evtc_bot/db/redis/models.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from enum import Enum
+
+from evtc_bot.db.redis.engine import redis_storage
+
+
+class UserRole(str, Enum):
+    admin = "admin"
+    user = "user"
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    role: UserRole
+
+    @classmethod
+    async def get_from_redis(cls, user_id: int):
+        user_data = await redis_storage.get(f"user:{user_id}")
+        if user_data:
+            return cls(**user_data)
+        else:
+            return None
+
+    async def save_to_redis(self):
+        await redis_storage.set(f"user:{self.id}", self.dict())
