4f8ddcc2226989fbf9b1805589d83112ed5371e4
Alexei Malinovskii <6328673@gmail.com>
2024-07-28 20:21:49 +0300

    doc: change comments

--- evtc_bot/utils/common.py
+++ evtc_bot/utils/common.py
@@ -4,8 +4,9 @@ import datetime as dt
 def get_now(dt_format: str) -> str:
     """
     Return the current date and time in a specific format as a string
-    :param dt_format:  specific format datetime
-    :return: current date and time in a specific format as a string
+
+    :param dt_format:  Specific format datetime
+    :return: Current date and time in a specific format as a string
     """
 
     now = dt.datetime.now()
