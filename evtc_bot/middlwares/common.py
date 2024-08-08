e7a61d5fe657a1cea3363915c7e25cf8e8c164e0
63286 <6328673@gmail.com>
2024-08-08 10:01:29 +0300

    refactor: optimize code

---
+++ evtc_bot/middlwares/common.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from aiogram.fsm.storage.base import StorageKey
+from aiogram.fsm.storage.redis import RedisStorage
+
+
+async def get_current_state(storage: RedisStorage, user_id: int, bot_id: int):
+    """
+    Get FSM state_key from storage for user_id, and bot_id
+    :param storage: storage
+    :param user_id: user id
+    :param bot_id: bot id
+    :return: state and state_key
+    """
+
+    state_key: StorageKey = StorageKey(
+        chat_id=user_id,
+        user_id=user_id,
+        bot_id=bot_id,
+    )
+    state = await storage.get_state(state_key)
+    return state, state_key
